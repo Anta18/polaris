74341edb45e66c18cfa9d28e503748af96b20579
AryanLoohia <aryanlohia2005@gmail.com>
2025-11-10 13:23:04 +0530

    fixed foder structure

--- llmproject/config.py
+++
@@ -1,8 +0,0 @@
-NEWS_API_KEY = "521c4f99d88642539efb085c9b68d01e"
-NUM_ARTICLES = 6
-EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-
-
-MONGO_URI="mongodb+srv://siddhantsingh15032005_db_user:NL8faLkskcXUa6Nb@polaris.jkw8jwe.mongodb.net/polaris_news_db?retryWrites=true&w=majority"
-MONGO_DB = "polaris_news_db"
-MONGO_COLLECTION = "articles"
